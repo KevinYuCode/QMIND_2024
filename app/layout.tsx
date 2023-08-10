399d2f068b066c4684f2bff4da4ec8550801ca8a
Kevin <yu.kevin2002@gmail.com>
2023-08-09 22:28:43 -0400

    fixed the margins

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,18 +39,41 @@ export default function RootLayout({
 }) {
   const [navOn, setNavOn] = useState(false);
   return (
-    <html lang="en">
-      <body
-        className={`${tradeGothic.variable} ${
-          sofia_sans.className
-        } ${"w-[100dvw] h-[100dvh]"} ${styles.mainBgColour}`}
-      >
-        <div className="flex flex-col w-[100dvw] h-[100dvh] overflow-scroll">
-          <Navbar navOn={navOn} setNavOn={setNavOn} />
-          <div className="pt-[100px]">{children}</div>
-          <Footer />
-        </div>
-      </body>
-    </html>
+    <>
+      <head>
+        <link
+          rel="shortcut icon"
+          href="%PUBLIC_URL%/cropped-New-QMIND-Logo-32x32.png"
+        />
+        <meta />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <title>QMIND - Queens AI Hub</title>
+        <meta property="og:title" content="QMIND - Queens AI Hub" />
+        <meta
+          name="description"
+          property="og:description"
+          content="QMIND is Canada’s largest student-run organization. We empower undergraduate student’s at Queen’s University to tackle real-world problems through the use of artificial intelligence, machine learning, and various other disruptive technologies."
+        />
+        <meta
+          property="og:image"
+          content="https://www.qmind.ca/preview_logo.jpg"
+        />
+        <meta property="og:url" content="https://www.qmind.ca" />
+      </head>
+      <html lang="en">
+        <body
+          className={`${tradeGothic.variable} ${
+            sofia_sans.className
+          } ${"w-[100dvw] h-[100dvh]"} ${styles.mainBgColour}`}
+        >
+          <div className="flex flex-col w-[100dvw] h-[100dvh] overflow-scroll">
+            <Navbar navOn={navOn} setNavOn={setNavOn} />
+            <div className="pt-[100px]">{children}</div>
+            <Footer />
+          </div>
+        </body>
+      </html>
+    </>
   );
 }
