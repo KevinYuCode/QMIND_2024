14416d685222e9c410670f58f37d5adfe0ca6630
Kevin <yu.kevin2002@gmail.com>
2023-08-09 22:32:41 -0400

    fixed cards

--- app/components/ContentContainer.tsx
+++ app/components/ContentContainer.tsx
@@ -4,7 +4,7 @@ import { ContentContainerProps } from "../interfaces/IContentContainer";
 function ContentContainer({ children, className }: ContentContainerProps) {
   return (
     <div
-      className={`${className} px-[15px] max-w-[380px] xs:max-w-[540px] sm:max-w-[550px] md:max-w-[720px] lg:max-w-[950px] xl:max-w-[1100px] 2xl:max-w-[1368px] mx-[auto]`}
+      className={`${className} px-[15px] max-w-[360px] xs:max-w-[490px] sm:max-w-[600px] md:max-w-[720px] lg:max-w-[950px] xl:max-w-[1100px] 2xl:max-w-[1368px] mx-[auto]`}
     >
       {children}
     </div>
