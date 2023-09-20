6da1e96f674fe3a0421427f32eb7d0eb5cfe7cc6
Kevin <yu.kevin2002@gmail.com>
2023-09-20 17:33:55 -0400

    helped ryan out because he has a thinkpad

--- app/page.tsx
+++ app/page.tsx
@@ -197,31 +197,33 @@ export default function Home() {
 
       {/* Stats card */}
       <ContentContainer>
-          {/* First Row */}
-          <div className="flex w-[100%] gap-[37px]">
-            {/*Top Row of the cards section */}
-            <div
-              className={`${styles.yellowCard} rounded-[17px] w-[100%] max-w-[768px] h-[180px] font-family: Kontrapunkt; flex flex-col justify-center items-start pl-[50px] leading-[50px]`}
-            >
-              <p className="lg:text-[65px] md:text-[45px]">230+</p>
-              <p className="lg:text-[32px] md:text-[25px]">
-                Software Developers
-              </p>
-            </div>
-            <div
-              className={`${styles.redCard}  rounded-[17px] w-[100%] max-w-[320px] h-[180px] font-family: Kontrapunkt; flex flex-col justify-center items-end pr-[50px] leading-[50px]`}
-            >
-              <p className="lg:text-[65px] md:text-[45px]">200+</p>
-              <p className="text-[32px] md:text-[22px]">AI papers & Projects</p>
-            </div>
+        {/* First Row */}
+        <div className="flex w-[100%] gap-[37px]">
+          {/*Top Row of the cards section */}
+          <div
+            className={`${styles.yellowCard} rounded-[17px] w-[100%]  h-[180px] font-family: Kontrapunkt; flex flex-col justify-center items-start pl-[50px] leading-[30px] md:leading-[50px]`}
+          >
+            <p className="text-[35px] md:text-[45px] lg:text-[65px]">230+</p>
+            <p className="text-[20px] md:text-[22px] text-left">
+              Software Developers
+            </p>
           </div>
-          {/*Middele row of the card section */}
-          {/* <div className="flex w-[100%] gap-[37px] ">
+          <div
+            className={`${styles.redCard}  rounded-[17px] w-[100%] max-w-[380px] h-[180px] font-family: Kontrapunkt; flex flex-col justify-center items-end pr-[50px] leading-[30px] md:leading-[50px]`}
+          >
+            <p className="text-[35px] md:text-[45px] lg:text-[65px]">200+</p>
+            <p className="text-[20px] md:text-[22px] text-right">
+              AI papers & Projects
+            </p>
+          </div>
+        </div>
+        {/*Middele row of the card section */}
+        {/* <div className="flex w-[100%] gap-[37px] ">
             <div
               className={`${styles.blueCard} rounded-[17px] w-[100%] h-[180px] font-family: Kontrapunkt; flex flex-col justify-center items-start pl-[50px] leading-[50px]`}
             >
               <p className="lg:text-[65px] md:text-[45px]">320+</p>
-              <p className="lg:text-[32px] md:text-[18px]">
+              <p className="lg:text-[25px] md:text-[18px]">
                 Delegates at CUCAI
               </p>
             </div>
@@ -230,29 +232,29 @@ export default function Home() {
               className={`${styles.yellowCard} rounded-[17px] w-[100%] h-[180px] font-family: Kontrapunkt; flex flex-col justify-center items-start pl-[50px] leading-[50px]`}
             >
               <p className="lg:text-[65px] md:text-[45px]">25+</p>
-              <p className="lg:text-[32px] md:text-[18px]">Industry Clients</p>
+              <p className="lg:text-[25px] md:text-[18px]">Industry Clients</p>
             </div>
 
             <div
               className={`${styles.blueCard} rounded-[17px] w-[100%] h-[180px] font-family: Kontrapunkt; flex flex-col justify-center items-end pr-[50px] leading-[50px]`}
             >
               <p className="lg:text-[65px] md:text-[45px]">45</p>
-              <p className="lg:text-[32px] md:text-[18px]">Tech Articles</p>
+              <p className="lg:text-[25px] md:text-[18px]">Tech Articles</p>
             </div>
           </div> */}
-          {/*Bottom row of the card section */}
-          {/* <div className="flex w-[100%] gap-[37px] ">
+        {/*Bottom row of the card section */}
+        {/* <div className="flex w-[100%] gap-[37px] ">
             <div
               className={`${styles.redCard} rounded-[17px] w-[100%] h-[180px] font-family: Kontrapunkt; flex flex-col justify-center items-start pl-[50px] leading-[50px]`}
             >
               <p className="lg:text-[65px] md:text-[45px]">35</p>
-              <p className="lg:text-[32px] md:text-[22px]">Projects in 2023</p>
+              <p className="lg:text-[25px] md:text-[22px]">Projects in 2023</p>
             </div>
             <div
               className={`${styles.yellowCard} rounded-[17px] w-[100%] h-[180px] font-family: Kontrapunkt; flex flex-col justify-center items-end pr-[50px] leading-[50px]`}
             >
               <p className="lg:text-[65px] md:text-[45px]">325+</p>
-              <p className="lg:text-[32px] md:text-[22px]">
+              <p className="lg:text-[25px] md:text-[22px]">
                 Hours Read on Medium
               </p>
             </div>
