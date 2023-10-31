d53b338d2cac0f1fca171a7e0df464a4b879001e
Kevin <yu.kevin2002@gmail.com>
2023-10-31 11:08:35 -0400

    fixed pillars

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Link from "next/link";
 import React from "react";
-import PILLARS from "../../assets/colourful-pillar.svg";
+import PILLARS from "../../assets/colourful-pillar.png";
 import ContentContainer from "./ContentContainer";
 import Image from "next/image";
 function Footer() {
