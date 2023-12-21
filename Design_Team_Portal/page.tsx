66bf23b0a45f278352bd89d3139992745b59e0b2
ryan-su-12 <21rzs@queensu.ca>
2023-12-21 12:54:30 -0500

    web dev section

---
+++ Design_Team_Portal/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+import Image from "next/image";
+
+import "./page.scss";
+//import { sofia_sans } from "../font";
+import Link from "next/link";
+
+
+export default function design_team_portal(){
+    return (
+        <div>hi</div>
+
+    );
+
+}
+
+
+
+  
