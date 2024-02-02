9f164bd61a9dd5de802496fbd3c443f6103a6f5a
kayne-lee <kayne.lee2@outlook.com>
2024-02-02 12:17:22 -0500

    pulling ryans

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-'use client'
-import React, { useState, useEffect } from 'react'
-import supabase from "../supabaseClient"
-
-
-function Projects() {
-    const [projects, setProjects] = useState<any>(null);
-
-    useEffect(() => {
-        const fetchData = async () => {
-    
-
-            const { data, error } = await supabase
-              .from('Projects')
-              .select('*')
-
-              //SELECT * FROM PROJECTS
-          
-            if (error) {
-              console.error('Error fetching data', error)
-              return
-            }
-            console.log(data)
-            setProjects(data);
-          }
-        fetchData();
-    },[]);
-
-  return (
-    <div>
-        {JSON.stringify(projects)}
-    </div>
-  )
-}
-
-export default Projects
-=======
 "use client";
 
 import React from 'react';
@@ -247,4 +209,3 @@ export default Users
 
 
   
->>>>>>> rs/icons
