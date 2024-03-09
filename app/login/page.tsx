6cd18cffee7b2e524a745433fddd2f181448cc1e
Kevin Yu (19KY6) <19ky6@queensu.ca>
2024-03-09 17:07:29 -0500

    test

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 export default async function LoginPage() {
   const supabase = createClient();
   const userRes = await supabase.auth.getUser();
-  console.log(userRes);
+  //@ts-ignore
   if (userRes?.user) redirect("/");
   return <LoginForm />;
 }
