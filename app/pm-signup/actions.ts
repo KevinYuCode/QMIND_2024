39e4a2f4a1e972a253de059c07193e49e7eae083
Kevin Yu (19KY6) <19ky6@queensu.ca>
2024-03-09 16:19:59 -0500

    testing signup bug

--- app/pm-signup/actions.ts
+++ app/pm-signup/actions.ts
@@ -1,50 +1,50 @@
-"use server";
+// "use server";
 
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
+// import { revalidatePath } from "next/cache";
+// import { redirect } from "next/navigation";
 
-import { createClient } from "@/utils/supabase/server";
+// import { createClient } from "@/utils/supabase/server";
 
-export async function login(formData: FormData) {
-  const supabase = createClient();
+// export async function login(formData: FormData) {
+//   const supabase = createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
-  };
+//   // type-casting here for convenience
+//   // in practice, you should validate your inputs
+//   const data = {
+//     email: formData.get("email") as string,
+//     password: formData.get("password") as string,
+//   };
 
-  const { error } = await supabase.auth.signInWithPassword(data);
+//   const { error } = await supabase.auth.signInWithPassword(data);
 
-  if (error) {
-    redirect("/error");
-  }
+//   if (error) {
+//     redirect("/error");
+//   }
 
-  revalidatePath("/", "layout");
-  redirect("/");
-}
+//   revalidatePath("/", "layout");
+//   redirect("/");
+// }
 
-export async function signup(formData: FormData) {
-  const supabase = createClient();
+// export async function signup(formData: FormData) {
+//   const supabase = createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
-    confirm_password: formData.get("confirm_password") as string,
-  };
+//   // type-casting here for convenience
+//   // in practice, you should validate your inputs
+//   const data = {
+//     email: formData.get("email") as string,
+//     password: formData.get("password") as string,
+//     confirm_password: formData.get("confirm_password") as string,
+//   };
 
-  if (data.password !== data.confirm_password)
-    redirect(`/signup_pms?error=password&email=${data.email}`);
+//   if (data.password !== data.confirm_password)
+//     redirect(`/signup_pms?error=password&email=${data.email}`);
 
-  const { error } = await supabase.auth.signUp(data);
+//   const { error } = await supabase.auth.signUp(data);
 
-  if (error) {
-    redirect("/error");
-  }
+//   if (error) {
+//     redirect("/error");
+//   }
 
-  revalidatePath("/", "layout");
-  redirect("/");
-}
+//   revalidatePath("/", "layout");
+//   redirect("/");
+// }
